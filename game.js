f0c8e90adf4f451cd9885cc46e14e640b8ee098a
shenaya <87794884+shenayalol@users.noreply.github.com>
2022-08-03 10:47:41 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,51 @@
+AFRAME.registerComponent("game", {
+    schema: {
+        gameState: { type: "string", default: "play" },
+      },
+
+    init: function () 
+    {
+        var duration = 300;
+        var timerEl = document.querySelector("#timer");
+        this.startTimer(duration, timerEl);
+    },
+
+    startTimer: function (duration, timerEl) {
+        var minutes;
+        var seconds;
+        setInterval(() => {
+            if (duration >= 0) {
+                this.data.gameState="play";
+                minutes = parseInt(duration / 60);
+                seconds = parseInt(duration % 60);
+
+                if (minutes < 10) //11
+                {
+                    minutes = "0" + minutes;   //0+11
+                }
+                if (seconds < 10)      
+                 {
+                    seconds = "0" + seconds;    
+                }
+
+                timerEl.setAttribute("text", {
+                    value: minutes + ":" + seconds,
+                });
+
+                duration -= 1;
+            }
+            else
+             {
+                this.data.gameState="over";
+                var cameraRig = document.querySelector("#camera-rig")
+                cameraRig.setAttribute("velocity", { x: 0, y: 0, z: 0 })   //setting velocity of camera as 0 
+                var over = document.querySelector("#over");                
+                over.setAttribute("visible",true)
+
+                var carSpeed = document.querySelector("#speed")
+                carSpeed.setAttribute("text", { value: "0" });
+            }
+
+        }, 100)
+    },
+})
